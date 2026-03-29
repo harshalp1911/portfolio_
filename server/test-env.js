975a3a0de37c029c789541433bf9a1da85dad6be
Harshal <harshalp0602@gmail.com>
2026-03-29 10:36:38 +0530

    Remove temporary test and debug files

--- server/test-env.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-console.log('Cloud Name:', process.env.CLOUDINARY_CLOUD_NAME);
-console.log('API Key:', process.env.CLOUDINARY_API_KEY);
-console.log('API Secret:', process.env.CLOUDINARY_API_SECRET);
-console.log('Email User:', process.env.EMAIL_USER);
-console.log('Email Pass:', process.env.EMAIL_PASS ? 'Set' : 'Not set');
