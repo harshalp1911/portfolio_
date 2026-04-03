2521f2f1b19f854d10ab21243938cc50925c6bd5
Harshal <harshalp0602@gmail.com>
2026-04-03 11:08:26 +0530

    Fix CORS for deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -26,7 +26,7 @@ const app: Application = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: [process.env.FRONTEND_URL || 'http://localhost:3000', 'http://localhost:3000', 'http://127.0.0.1:3000'],
+  origin: true,
   credentials: true
 }));
 app.use(express.json());
